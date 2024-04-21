c710a4f9f6294090b5047f7c330ea23c65f3ab2e
manoj <msbarhate14@gmail.com>
2024-04-21 13:03:53 +0530

    Version v1.1

--- src/test/java/pageObject/LoginPageWithPageFactory.java
+++ src/test/java/pageObject/LoginPageWithPageFactory.java
@@ -1,15 +1,46 @@
 package pageObject;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 //Page Object model with page factory
 public class LoginPageWithPageFactory {
-	
+
 	WebDriver ldriver;
-	
-	public LoginPageWithPageFactory(WebDriver driver)
-	{
-		ldriver=driver;
-		
+
+	public LoginPageWithPageFactory(WebDriver driver) {
+		ldriver = driver;
+
 	}
 
+	@FindBy(id = "Email")
+	WebElement email;
+
+	@FindBy(id = "Password")
+	WebElement password;
+
+	@FindBy(xpath = "//button[@class='button-1 login-button']")
+	WebElement LoginBtn;
+
+	@FindBy(linkText = "Logout")
+	WebElement logout;
+
+	public void enterEmail(String emailAdd) {
+		email.clear();
+		email.sendKeys(emailAdd);
+	}
+
+	public void enterPassword(String pwd) {
+		password.clear();
+		password.sendKeys(pwd);
+	}
+
+	public void clickOnLoginButton() {
+		LoginBtn.click();
+	}
+
+	public void clickOnLogOutButton() {
+		logout.click();
+	}
 }
--- src/test/java/pageObject/LoginPageWithoutPF.java
+++ src/test/java/pageObject/LoginPageWithoutPF.java
@@ -1,5 +1,10 @@
 package pageObject;
 
+// Page Object model without page factory
 public class LoginPageWithoutPF {
+	
+	
+	
+	
 
 }
